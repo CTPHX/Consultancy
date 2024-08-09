8a6d82ea1ba1abc2ad68cac7b2601659f5248a22
aidiewright <79138553+aidiewright@users.noreply.github.com>
2024-08-09 15:23:55 +0100

    Update installapps.ps1

--- installapps.ps1
+++ installapps.ps1
@@ -1,5 +1,7 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
+choco feature enable -n allowglobalconfimration
+
 choco install python
 choco install vscode
 choco install qgis
