92ab551b57a96390597d24555ca8acad26d2546b
aidiewright <79138553+aidiewright@users.noreply.github.com>
2024-08-09 12:13:59 +0100

    Create installapps.ps1

---
+++ installapps.ps1
@@ -0,0 +1,6 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+choco install python
+choco install vscode
+choco install qgis
+choco install r.studio
