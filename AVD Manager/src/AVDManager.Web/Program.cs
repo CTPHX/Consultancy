5270e32579682f0fbce47945f7b240b29dcb65d9
aidiewright <79138553+aidiewright@users.noreply.github.com>
2026-09-03 14:36:16 +0100

    Add AVD Manager application entry point

---
+++ AVD Manager/src/AVDManager.Web/Program.cs
@@ -0,0 +1,20 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+builder.Services.AddHttpContextAccessor();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+app.MapRazorPages();
+
+app.Run();
