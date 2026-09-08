892f22fcfb60e05b67bf946879c2f4d993e02399
aidiewright <79138553+aidiewright@users.noreply.github.com>
2026-09-08 11:16:51 +0100

    Register host pool detail service

--- AVD Manager/src/AVDManager.Web/Program.cs
+++ AVD Manager/src/AVDManager.Web/Program.cs
@@ -54,6 +54,7 @@ builder.Services.AddSingleton<TokenCredential>(sp =>
 builder.Services.AddScoped<AVDManager.Web.Services.AzureDiscoveryService>();
 builder.Services.AddScoped<AVDManager.Web.Services.AzureVmImageDiscoveryService>();
 builder.Services.AddScoped<AVDManager.Web.Services.HostPoolRefreshService>();
+builder.Services.AddScoped<AVDManager.Web.Services.HostPoolDetailService>();
 builder.Services.AddSingleton<AVDManager.Web.Services.EnvironmentConfigurationStore>();
 
 var app = builder.Build();
