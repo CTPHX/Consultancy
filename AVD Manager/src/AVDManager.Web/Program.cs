3adc9413d3e8fb5ec5b5793c2089111a11d880f3
aidiewright <79138553+aidiewright@users.noreply.github.com>
2026-09-11 15:17:51 +0100

    Register durable deployment orchestration worker

--- AVD Manager/src/AVDManager.Web/Program.cs
+++ AVD Manager/src/AVDManager.Web/Program.cs
@@ -49,6 +49,7 @@ builder.Services.AddScoped<AVDManager.Web.Services.AzureVmOperationsService>();
 builder.Services.AddScoped<AVDManager.Web.Services.AzureAutomationService>();
 builder.Services.AddSingleton<AVDManager.Web.Services.EnvironmentConfigurationStore>();
 builder.Services.AddSingleton<AVDManager.Web.Services.DeploymentOperationStore>();
+builder.Services.AddHostedService<AVDManager.Web.Services.DeploymentOrchestrationWorker>();
 
 var app = builder.Build();
 app.UseForwardedHeaders();
